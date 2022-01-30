01a3c7c8a8af73395f1478d96173be2e0f420150
Christophe Laudon <ch.laudon@gmail.com>
2022-01-30 16:20:41 +0100

    :hammer: Refactoring

--- module/cthack.js
+++ module/cthack.js
@@ -19,7 +19,7 @@ import { Macros } from "./macros.js";
 import * as chat from "./chat.js";
 
 import { GMManager } from './app/gm-manager.js';
-//import { Die4Cthack, Die6Cthack } from './dice.js';
+import { configureDiceSoNice } from './dice.js';
 
 Hooks.once('init', async function() {
 	console.log(LOG_HEAD + 'Initializing the Cthulhu Hack Game System');
@@ -198,132 +198,7 @@ Hooks.on("hotbarDrop", async (bar, data, slot) => {
 	}
 });
 
-/*
-Hooks.on("renderPause", (_app, html) => {
-	html.find("img").attr("src", "systems/cthack/ui/cthack-logo.webp");
-});
-*/
-
 // Dice-so-nice Ready
 Hooks.once('diceSoNiceReady', (dice3d) => {
-		/*Die6Cthack.diceSoNiceReady(dice3d);
-		Die4Cthack.diceSoNiceReady(dice3d);
-		*/
-
-		dice3d.addSystem({id: "cthack", name: "Cthulhu Hack"}, "preferred");
-
-		dice3d.addColorset({
-			name: 'cthack',
-			description: "Cthulhu Hack Default",
-			category: "Cthulhu Hack",
-			foreground: '#000000',
-			background: "#000000",
-			texture: 'none',
-			edge: '#000000',
-			material: 'plastic',
-			visibility: 'visible'
-		},"preferred");		
-
-		dice3d.addDicePreset({
-            type: "d4",
-            labels: [
-                "systems/cthack/ui/dice/d4/d4-1.webp",
-                "systems/cthack/ui/dice/d4/d4-2.webp",
-                "systems/cthack/ui/dice/d4/d4-3.webp",
-                "systems/cthack/ui/dice/d4/d4-4.webp",
-            ],
-            system: "cthack"
-        });
-
-		dice3d.addDicePreset({
-            type: "d6",
-            labels: [
-                "systems/cthack/ui/dice/d6/d6-1.webp",
-                "systems/cthack/ui/dice/d6/d6-2.webp",
-                "systems/cthack/ui/dice/d6/d6-3.webp",
-                "systems/cthack/ui/dice/d6/d6-4.webp",
-                "systems/cthack/ui/dice/d6/d6-5.webp",
-                "systems/cthack/ui/dice/d6/d6-6.webp"
-            ],
-            system: "cthack"
-        });	
-
-		dice3d.addDicePreset({
-            type: "d8",
-            labels: [
-                "systems/cthack/ui/dice/d8/d8-1.webp",
-                "systems/cthack/ui/dice/d8/d8-2.webp",
-                "systems/cthack/ui/dice/d8/d8-3.webp",
-                "systems/cthack/ui/dice/d8/d8-4.webp",
-				"systems/cthack/ui/dice/d8/d8-5.webp",
-				"systems/cthack/ui/dice/d8/d8-6.webp",
-				"systems/cthack/ui/dice/d8/d8-7.webp",
-				"systems/cthack/ui/dice/d8/d8-8.webp"
-            ],
-            system: "cthack"
-        });
-
-		dice3d.addDicePreset({
-            type: "d10",
-            labels: [
-                "systems/cthack/ui/dice/d10/d10-1.webp",
-                "systems/cthack/ui/dice/d10/d10-2.webp",
-                "systems/cthack/ui/dice/d10/d10-3.webp",
-                "systems/cthack/ui/dice/d10/d10-4.webp",
-				"systems/cthack/ui/dice/d10/d10-5.webp",
-				"systems/cthack/ui/dice/d10/d10-6.webp",
-				"systems/cthack/ui/dice/d10/d10-7.webp",
-                "systems/cthack/ui/dice/d10/d10-8.webp",
-				"systems/cthack/ui/dice/d10/d10-9.webp",
-				"systems/cthack/ui/dice/d10/d10-10.webp"
-            ],
-            system: "cthack"
-        });
-
-		dice3d.addDicePreset({
-            type: "d12",
-            labels: [
-                "systems/cthack/ui/dice/d20/d20-1.webp",
-                "systems/cthack/ui/dice/d20/d20-2.webp",
-                "systems/cthack/ui/dice/d20/d20-3.webp",
-                "systems/cthack/ui/dice/d20/d20-4.webp",
-                "systems/cthack/ui/dice/d20/d20-5.webp",
-				"systems/cthack/ui/dice/d20/d20-6.webp",
-				"systems/cthack/ui/dice/d20/d20-7.webp",
-				"systems/cthack/ui/dice/d20/d20-8.webp",
-				"systems/cthack/ui/dice/d20/d20-9.webp",
-				"systems/cthack/ui/dice/d20/d20-10.webp",
-				"systems/cthack/ui/dice/d20/d20-11.webp",
-				"systems/cthack/ui/dice/d20/d20-12.webp",
-            ],
-            system: "cthack"
-        });	
-
-		dice3d.addDicePreset({
-            type: "d20",
-            labels: [
-                "systems/cthack/ui/dice/d20/d20-1.webp",
-                "systems/cthack/ui/dice/d20/d20-2.webp",
-				"systems/cthack/ui/dice/d20/d20-3.webp",
-				"systems/cthack/ui/dice/d20/d20-4.webp",
-				"systems/cthack/ui/dice/d20/d20-5.webp",
-				"systems/cthack/ui/dice/d20/d20-6.webp",
-				"systems/cthack/ui/dice/d20/d20-7.webp",
-				"systems/cthack/ui/dice/d20/d20-8.webp",
-				"systems/cthack/ui/dice/d20/d20-9.webp",
-				"systems/cthack/ui/dice/d20/d20-10.webp",
-				"systems/cthack/ui/dice/d20/d20-11.webp",
-				"systems/cthack/ui/dice/d20/d20-12.webp",
-				"systems/cthack/ui/dice/d20/d20-13.webp",
-				"systems/cthack/ui/dice/d20/d20-14.webp",
-				"systems/cthack/ui/dice/d20/d20-15.webp",
-				"systems/cthack/ui/dice/d20/d20-16.webp",
-				"systems/cthack/ui/dice/d20/d20-17.webp",
-				"systems/cthack/ui/dice/d20/d20-18.webp",
-				"systems/cthack/ui/dice/d20/d20-19.webp",
-				"systems/cthack/ui/dice/d20/d20-20.webp"
-            ],
-            system: "cthack"
-        });			
-		
+	configureDiceSoNice(dice3d);		
 });
